7a6ef7b5425f7cac0af7692b4dd6d56a6927ccd4
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 18:42:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logDriversByHometown(drivers, location) {
 }
 
 function driversByRevenue(drivers) {
-  return drivers.sort((a, b) => {
+  return drivers.slice().sort((a, b) => {
     a.revenue = b.revenue
   })
 }
