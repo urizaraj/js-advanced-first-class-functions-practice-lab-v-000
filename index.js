db8787d8bc5e50ef1a6a87f4ca19edcab49d54a2
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 18:38:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function logDriverNames(drivers) {
 
 function logDriversByHometown(drivers, location) {
   drivers.forEach((driver) => {
-    if (driver.location === location) {
+    if (driver.hometown === location) {
       console.log(driver.name)
     }
   })
